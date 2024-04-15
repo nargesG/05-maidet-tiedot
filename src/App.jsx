92bce2963cdb1039988fb2af7d3b636d698b7d51
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-04-16 00:00:23 +0300

    Get data from countries step 1 -part 2.18

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import countryServices from './services/countryServices';
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filterString, setFilterString] = useState('');
+
+  const handleSearchChange = async event => {
+    const value = event.target.value;
+    setFilterString(value);
+
+    // Fetch countries only when user starts typing
+    try {
+      const fetchedCountries = await countryServices.getAll();
+
+      // Check if number of filtered countries exceeds 10
+      const matchedCountries = fetchedCountries.filter(
+        (country) => country.name.common.toLowerCase().includes(value.toLowerCase())
+      )
+
+      console.log(matchedCountries);
+      setCountries(matchedCountries);
+
+    } catch (error) {
+      console.error('Error fetching countries:', error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <label>Find countries</label>
+      <input value={filterString} onChange={handleSearchChange} />
 
-export default App
+      {countries.length > 10 && 
+        <p>Too many matches, specify another filter</p>
+      }
+
+      {(countries.length > 1 && countries.length <= 10) && 
+        countries.map(({ name }) => <p key={name.common}>{name.common}</p>)
+      }
+
+      {countries.length === 1 && 
+        <div>
+          <h1>{countries[0].name.common}</h1>
+          <p>Capital: {countries[0].capital[0]}</p>
+          <p>Area: {countries[0].area}</p>
+          <h2>Languages:</h2>
+          <ul>
+          {Object.keys(countries[0].languages).map((key) => 
+            <li key={key}>{countries[0].languages[key]}</li>
+          )
+          }
+          </ul>
+          <img alt={countries[0].flags.alt} src={countries[0].flags.svg} width={200} height={150}/>
+          
+        </div>
+      }
+      
+    </>
+  );
+};
+
+export default App;
