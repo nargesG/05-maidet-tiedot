7561117f5c5671042bb9f06a39d36ef59540828b
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-04-16 18:19:56 +0300

    Add show button for short view of selected country -step 2 -part 2.19

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,81 @@
 import countryServices from './services/countryServices';
 import { useState } from 'react';
+import "./index.css";
+import { useEffect } from 'react';
+
 
 const App = () => {
+  const [all, setAll] = useState([]);
+
   const [countries, setCountries] = useState([]);
   const [filterString, setFilterString] = useState('');
 
+  useEffect(() => {
+    const call = async () => {
+      try {
+        const fetchedCountries = await countryServices.getAll();
+        setAll(fetchedCountries)
+        
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    }
+    call();
+    
+  }, [])
+  
   const handleSearchChange = async event => {
     const value = event.target.value;
     setFilterString(value);
-
-    // Fetch countries only when user starts typing
-    try {
-      const fetchedCountries = await countryServices.getAll();
-
-      // Check if number of filtered countries exceeds 10
-      const matchedCountries = fetchedCountries.filter(
-        (country) => country.name.common.toLowerCase().includes(value.toLowerCase())
-      )
-
-      console.log(matchedCountries);
-      setCountries(matchedCountries);
-
-    } catch (error) {
-      console.error('Error fetching countries:', error);
-    }
+    
+    const matchedCountries = all.filter(
+      (country) => country.name.common.toLowerCase().includes(value.toLowerCase())
+    )
+    setCountries(matchedCountries);
   };
+
+  
+  const ShortView = ({ country }) => {
+    return (
+      <div>
+          <h1>{country.name.common}</h1>
+          <p>Capital: {country.capital[0]}</p>
+          <p>Area: {country.area}</p>
+          <h2>Languages:</h2>
+          <ul>
+          {Object.keys(country.languages).map((key) => 
+            <li key={key}>{country.languages[key]}</li>
+          )}
+        </ul>
+        <img alt={country.flags.alt} src={country.flags.svg} width={200} height={150}/>
+                    
+        </div>
+    )
+
+  }
 
   return (
     <>
-      <label>Find countries</label>
-      <input value={filterString} onChange={handleSearchChange} />
+      <div className='searchCountries'>
+        <label>Find countries</label>
+        <input value={filterString} onChange={handleSearchChange} />
+      </div>
 
       {countries.length > 10 && 
         <p>Too many matches, specify another filter</p>
       }
 
       {(countries.length > 1 && countries.length <= 10) && 
-        countries.map(({ name }) => <p key={name.common}>{name.common}</p>)
+        countries.map((country) => (<div key={country.name.common}>
+          <p>
+            {country.name.common}
+            <button onClick={() => setCountries([country])}>Show</button>
+          </p>
+        </div>
+        ))
       }
 
-      {countries.length === 1 && 
-        <div>
-          <h1>{countries[0].name.common}</h1>
-          <p>Capital: {countries[0].capital[0]}</p>
-          <p>Area: {countries[0].area}</p>
-          <h2>Languages:</h2>
-          <ul>
-          {Object.keys(countries[0].languages).map((key) => 
-            <li key={key}>{countries[0].languages[key]}</li>
-          )
-          }
-          </ul>
-          <img alt={countries[0].flags.alt} src={countries[0].flags.svg} width={200} height={150}/>
-          
-        </div>
-      }
+      {countries.length === 1 && <ShortView country={countries[0]} /> }
       
     </>
   );
