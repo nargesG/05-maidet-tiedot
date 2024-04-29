83802835b857eb9e97dbe1276e31dcb9ea6bf089
Narges Ghanbari <narges.ghanbari@gmail.com>
2024-04-29 23:33:58 +0300

    Make .env file

--- src/services/weatherServices.jsx
+++ src/services/weatherServices.jsx
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const getWeather = (lat, lon) => {
-  const apiKey = '562aa7a775b43a43e788a9c62fba043d';
   const request = axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
   );
   return request.then((response) => {
     return response.data;
